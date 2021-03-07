5dc0983967d066f3709aa6e01ff51616f3acad64
gide segid <gidesegid@gides-MacBook-Air.local>
2021-03-07 13:00:56 +0100

    Remove duplicated directory

--- backend/routes/main.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const bodyParser=require('body-parser');
-const controller=require('../controller/controller').controller
-router.get('/',controller.getAllUsers)
-router.get('/user/:id',controller.getDetail)
-module.exports=router
